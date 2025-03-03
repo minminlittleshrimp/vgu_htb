8c9f71250f35f792a232e18cf0f6d33b10c7202c
minminlittleshrimp <minhmark47@gmail.com>
2025-03-04 01:14:12 +0700

    Init
    Signed-off-by: minminlittleshrimp <minhmark47@gmail.com>

---
+++ lab_tasks/dlt_vuln/build/CMakeFiles/dlt_vuln.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dlt_vuln.dir/dlt_vuln.c.o"
+  "CMakeFiles/dlt_vuln.dir/dlt_vuln.c.o.d"
+  "dlt_vuln"
+  "dlt_vuln.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/dlt_vuln.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
