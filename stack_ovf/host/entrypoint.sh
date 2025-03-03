cd97fd416114197cad235cfb9888032b97c7f581
minminlittleshrimp <minhmark47@gmail.com>
2025-03-04 04:38:00 +0700

    Apply new things
    Signed-off-by: minminlittleshrimp <minhmark47@gmail.com>

--- stack_ovf/host/entrypoint.sh
+++ stack_ovf/host/entrypoint.sh
@@ -3,4 +3,4 @@
 echo "vgucypher{w3lc0m3_t0_th3_h1dd3n_sh3ll}" > /app/flag.txt
 
 # Start the challenge
-exec socat TCP-LISTEN:1337,fork EXEC:/app/hidden_shell
+exec socat TCP-LISTEN:1337,fork EXEC:/app/stack_ovf
