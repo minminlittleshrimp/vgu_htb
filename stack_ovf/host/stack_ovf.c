4e91c9894e332d65593cad79c21f89f1d45951ee
minminlittleshrimp <minhmark47@gmail.com>
2025-03-05 03:31:47 +0700

    Fix big bug: incompatible OS version 20-24 LTS
    Signed-off-by: minminlittleshrimp <minhmark47@gmail.com>

--- stack_ovf/host/stack_ovf.c
+++ stack_ovf/host/stack_ovf.c
@@ -1,22 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <time.h>
+#include <fcntl.h>
+#include <sys/sendfile.h>
 
-void hidden() {
-    system("/bin/bash");
+int win() {
+    printf("You win! Here is your shell:\n");
+
+    system("/bin/sh");
 }
 
-void vulnerable() {
-    char buffer[32]; // Small buffer on the stack
+int get_favorite_food() {
+    char food[64]= {0};
 
-    printf("Enter your input: ");
+    printf("What is your favorite food?\n");
     fflush(stdout);
-    gets(buffer); // Unsafe function, vulnerable to buffer overflow
 
-    printf("You entered: %s\n", buffer);
+    fgets(food, 100, stdin);
+
+    printf("Hmmm..... %s...", food);
 }
 
 int main() {
-    vulnerable();
+    int rand_num;
+
+    srand(time(0));
+    rand_num = rand() % 100;
+
+    get_favorite_food();
+
+    if (rand_num <= 50) {
+        printf("That sounds delicious!\n");
+    } else if (rand_num <= 70) {
+        printf("Eh, that sounds okay.\n");
+    } else if (rand_num <= 80) {
+        printf("That's my favorite food too!\n");
+    } else if (rand_num <= 90) {
+        printf("I've never tried that before!\n");
+    } else if (rand_num <= 100) {
+        printf("Ew! I would never eat that.\n");
+    }
+
     return 0;
 }
